709f0d442d7e1b27a0dd551cea3317c3fc4eb7e6
akxrepo <mails.ashok@yahoo.com>
2025-10-10 18:15:58 +1100

    add alb pod identity

--- eks-managed/4-pod-identity.tf
+++ eks-managed/4-pod-identity.tf
@@ -39,6 +39,11 @@ resource "aws_eks_pod_identity_association" "karpenter_pod_identity" {
       namespace            = "kube-system"
       service_account_name = "karpenter"
       role_arn             = aws_iam_role.karpenter_role.arn
+    },
+    alb = {
+      namespace            = "kube-system"
+      service_account_name = "aws-load-balancer-controller"
+      role_arn             = aws_iam_role.aws_load_balancer_controller_role.arn
     }
   }
 
@@ -48,4 +53,36 @@ resource "aws_eks_pod_identity_association" "karpenter_pod_identity" {
   cluster_name    = var.eks_cluster_name
 
   depends_on = [aws_eks_addon.essential_addons]
+}
+
+# ALB
+resource "aws_iam_role" "aws_load_balancer_controller_role" {
+  name = "eks-${var.environment}-ALB-Controller-Role"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "pods.eks.amazonaws.com"
+        },
+        "Action": [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller_policy" {
+  name        = "eks-${var.environment}-AWSLoadBalancerControllerIAMPolicy"
+  description = "IAM policy for the AWS Load Balancer Controller"
+  policy      = file("alb-iam-policy.json")
+}
+
+resource "aws_iam_policy_attachment" "alb_controller_attach_policy" {
+  name       = "eks-${var.environment}-alb-controller-attach-policy"
+  policy_arn = aws_iam_policy.aws_load_balancer_controller_policy.arn
+  roles      = [aws_iam_role.aws_load_balancer_controller_role.name]
 }
