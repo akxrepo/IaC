f89f1b25ff221b3c358d55cd425bc9ba1cbfd699
akxrepo <mails.ashok@yahoo.com>
2025-10-10 12:13:54 +1100

    fix typo in karpenter role json

--- eks-managed/4-pod-identity.tf
+++ eks-managed/4-pod-identity.tf
@@ -5,20 +5,15 @@ resource "aws_iam_role" "karpenter_role" {
     Version = "2012-10-17"
     Statement = [
       {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "pods.eks.amazonaws.com"
-            },
-            "Action": [
-                "sts:AssumeRole",
-                "sts:TagSession"
-            ]
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
         }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
         ]
-    }
+      }
     ]
   })
 
@@ -36,23 +31,21 @@ resource "aws_iam_policy" "karpenter_policy" {
 resource "aws_iam_role_policy_attachment" "karpenter_attach_policy" {
   role       = aws_iam_role.karpenter_role.name
   policy_arn = aws_iam_policy.karpenter_policy.arn
-  
 }
 
 resource "aws_eks_pod_identity_association" "karpenter_pod_identity" {
   for_each = {
     karpenter = {
-      namespace      = "karpenter"
+      namespace            = "karpenter"
       service_account_name = "karpenter"
-      role_arn            = aws_iam_role.karpenter_role.arn
+      role_arn             = aws_iam_role.karpenter_role.arn
     }
   }
 
-  namespace             = each.value.namespace
-  service_account       = each.value.service_account_name
-  role_arn              = each.value.role_arn
-  cluster_name = var.eks_cluster_name
+  namespace       = each.value.namespace
+  service_account = each.value.service_account_name
+  role_arn        = each.value.role_arn
+  cluster_name    = var.eks_cluster_name
 
   depends_on = [aws_eks_addon.essential_addons]
-  
 }
