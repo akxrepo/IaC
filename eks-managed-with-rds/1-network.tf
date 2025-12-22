33643ad3c405b7a1e693888c9f6221e2ee950c32
akxrepo <mails.ashok@yahoo.com>
2025-12-22 17:47:23 +1100

    add sg for testing

--- eks-managed-with-rds/1-network.tf
+++ eks-managed-with-rds/1-network.tf
@@ -174,3 +174,28 @@ resource "aws_security_group" "alb-to-ec2-sg" {
         Name = "EKS-${var.environment}-alb-to-ec2-sg"
     }
 }
+
+
+resource "aws_security_group" "allow-all-sg" {
+  name        = "allow-all-sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.test-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "EKS-${var.environment}-allow-all-sg"
+  }
+}
