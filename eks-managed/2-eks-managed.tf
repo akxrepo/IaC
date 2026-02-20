442bb05fe102235f11eb999191f4aa8e5baf292c
akxrepo <mails.ashok@yahoo.com>
2026-02-20 15:42:28 +1100

    allow inbound traffic from vpc_cidr

--- eks-managed/2-eks-managed.tf
+++ eks-managed/2-eks-managed.tf
@@ -283,6 +283,16 @@ resource "aws_security_group" "eks_node_sg" {
   dynamic "ingress" {
     for_each = toset([80, 443])
     content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = [aws_vpc.test-vpc.cidr_block]
+    }
+  }
+
+  dynamic "ingress" {
+    for_each = toset([80, 443])
+    content {
       from_port       = ingress.value
       to_port         = ingress.value
       protocol        = "tcp"
