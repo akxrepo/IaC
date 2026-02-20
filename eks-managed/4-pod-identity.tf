165efc80c3cf3279f6b5a590abf7731942da6460
akxrepo <mails.ashok@yahoo.com>
2026-02-20 14:22:31 +1100

    update ebs pod identity role

--- eks-managed/4-pod-identity.tf
+++ eks-managed/4-pod-identity.tf
@@ -130,3 +130,9 @@ resource "aws_iam_role_policy_attachment" "ebs_csi_driver_attach_policy" {
   role       = aws_iam_role.ebs_csi_driver_role.name
   policy_arn = aws_iam_policy.ebs_csi_driver_policy.arn
 }
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_attach_policy" {
+  role       = aws_iam_role.ebs_csi_driver_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  
+}
